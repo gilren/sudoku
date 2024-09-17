f8bbd66c741470e42dedacb29acbdb6da496417f
gilren <gillet.ren@gmail.com>
2024-09-17 14:04:09 +0200

    Switching from parcel to vite

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import './style.css';
+
 import Sudoku from './Sudoku';
 
-const aSudoku = new Sudoku(document.querySelector('.sudoku'));
+const element = document.getElementById('app');
+
+if (element) {
+  const sudokuDiv = element as HTMLElement;
+  new Sudoku(sudokuDiv);
+} else {
+  console.error('Element with id app not found');
+}
